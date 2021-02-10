fb0eceeb357b5149527e33faf2cfe97ef8bb0c6a
Frank Ørnehaug <77120650+fornehaug@users.noreply.github.com>
2021-02-10 18:10:01 +0100

    Versjon 1

---
+++ script.js
@@ -0,0 +1,10 @@
+// Javascript Navbar Start
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+// Javascript Navbar Slutt
